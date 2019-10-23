29755bb4a72ca73de1509896e47c3ac7e175ccca
Alexis Paques <alexis@thekobi.com>
2019-10-23 14:13:59 +0200

    Disable the quaternion

--- src/modules/mavlink/mavlink_main.cpp
+++ src/modules/mavlink/mavlink_main.cpp
@@ -1758,7 +1758,7 @@ Mavlink::configure_streams_to_default(const char *configure_single_stream)
 	case MAVLINK_MODE_CUSTOM:
 		configure_stream_local("ALTITUDE", 10.0f);
 		configure_stream_local("ATTITUDE", unlimited_rate);
-		configure_stream_local("ATTITUDE_QUATERNION", unlimited_rate);
+		// configure_stream_local("ATTITUDE_QUATERNION", unlimited_rate);
 		configure_stream_local("CAMERA_TRIGGER", unlimited_rate);
 		configure_stream_local("ESTIMATOR_STATUS", 1.0f);
 		configure_stream_local("EXTENDED_SYS_STATE", 1.0f);
